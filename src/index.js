c6e5cdc8b7f3a70a8355e5e26e7919d2424752de
Rohan GB <114596598+RGB16@users.noreply.github.com>
2023-09-28 14:53:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import { Provider } from "react-redux";
 import { store } from './store';
 
-axios.defaults.baseURL ="https://api.render.com/deploy/srv-ckaj4s4g66mc7382vi90?key=Y0Cy2WSSVq8"; //"http://localhost:5000";
+axios.defaults.baseURL ="https://api.render.com/deploy/srv-ckaj4s4g66mc7382vi90?key=Y0Cy2WSSVq8"||"http://localhost:5000" ;//"http://localhost:5000";
 
 ReactDOM.render(
   <React.StrictMode>
